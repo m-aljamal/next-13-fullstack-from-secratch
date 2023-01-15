fad195e513b2912df61a9269875726a8468fbd57
m-aljamal <mohammadjamol@gmail.com>
2023-01-15 12:08:43 +0300

    projects

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { hashPassword } from "./../lib/auth";
 import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
 
@@ -17,7 +18,7 @@ async function main() {
     create: {
       email: "user@email.com",
       name: "User",
-      password: "password",
+      password: await hashPassword("password"),
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
